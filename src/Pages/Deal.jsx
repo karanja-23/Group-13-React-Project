985b11fdb203c11ace21c72719d2e957fc8ea1f7
Hosea <karanja.mungai23@gmail.com>
2024-11-13 21:00:04 +0300

    added homepage

--- src/Pages/Deal.jsx
+++ src/Pages/Deal.jsx
@@ -5,6 +5,9 @@ function Deal (){
             <NavBar/>
             <ol style={{width: "80%"}}>
                 <li>Both the dealer and player(s) are dealt two cards, but the dealer only shows one cardwhile the other is kept face down. <span style={{textDecoration: "underline", textUnderlineOffset: "0.2em"}}>The player always goes first</span></li>
+                <div className="step-demo">
+                    
+                </div>
             </ol>
         </div>
     )

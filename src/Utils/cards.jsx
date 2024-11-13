a1ce197db60d36746c684fd1a89044525c7f609e
James Maroko <jamiemaroko41@gmail.com>
2024-11-13 12:41:31 +0300

    Updated

--- src/Utils/cards.jsx
+++ src/Utils/cards.jsx
@@ -1,11 +1,39 @@
+// Import the suit images
+import suitSpade from "../assets/suit.png";
+import suitHeart from "../assets/suit.png";
+import suitDiamond from "../assets/suit.png";
+import suitClub from "../assets/suit.png";
 
+function Cards({ image, number, width, height, fontSize, imageWidth, imageHeight, suit }) {
+  // Map suit string to the corresponding image
+  const suitImageMap = {
+    "♠": suitSpade,
+    "♥": suitHeart,
+    "♦": suitDiamond,
+    "♣": suitClub,
+  };
 
-function Cards({ image, number, width, height, fontSize, imageWidth, imageHeight }) {
   return (
     <div className="cards-container" style={{ width, height }}>
-      { number === "" ? (<img src={image} style={{width:"89%", height:"87%", marginTop:"9%"}}></img>): (<img style={{width: imageWidth, height: imageHeight}} className="suit-image" src={image} />)}
-      <p style={{fontSize: fontSize}} className="rightside-up-number">{number}</p>
-      <p style={{fontSize: fontSize}} className="upside-down-number">{number}</p>
+      {number === "" ? (
+        <img src={image} style={{ width: "89%", height: "87%", marginTop: "9%" }} alt="Card back" />
+      ) : (
+        <>
+          {/* Display the suit image inside the card */}
+          <img
+            style={{ width: imageWidth, height: imageHeight }}
+            className="suit-image"
+            src={suitImageMap[suit]} // Use the correct suit image
+            alt="Suit"
+          />
+          <p style={{ fontSize }} className="rightside-up-number">
+            {number}
+          </p>
+          <p style={{ fontSize }} className="upside-down-number">
+            {number}
+          </p>
+        </>
+      )}
     </div>
   );
 }
@@ -15,7 +43,7 @@ Cards.defaultProps = {
   height: "150px",
   fontSize: "1.1em",
   imageWidth: "75%",
-  imageHeight: "60%"
+  imageHeight: "60%",
 };
 
 export default Cards;
